84ad3e73b7982606b9ce659b697c660edf681ea5
Joao Meyer <1994meyer@gmail.com>
2020-10-05 21:53:25 -0300

    Now we pass cluster by name

--- infra/service/data.tf
+++ infra/service/data.tf
@@ -10,3 +10,8 @@ data "aws_region" "current" {}
 data "aws_subnet_ids" "subnets" {
   vpc_id = data.aws_vpc.default.id
 }
+
+
+data "aws_ecs_cluster" "cluster" {
+  cluster_name = var.cluster
+}
--- infra/service/service.tf
+++ infra/service/service.tf
@@ -1,7 +1,7 @@
 
 resource "aws_ecs_service" "service" {
   name            = "service"
-  cluster         = var.cluster
+  cluster         = data.aws_ecs_cluster.cluster.arn
   task_definition = aws_ecs_task_definition.task.arn
   launch_type     = "FARGATE"
   desired_count   = 1
